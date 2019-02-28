bd59994e3d81f70a00e2b022a00d46e1d4d20bf1
Unknown <louismaximepiton@gmail.com>
2019-02-28 19:10:45 +0100

    A example

--- Projet/src/com/company/Main.java
+++ Projet/src/com/company/Main.java
@@ -1,8 +1,16 @@
-package com.company;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.File;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+    	try{
+    		File fichier = new File("./A.txt");
+    		fichier.createNewFile();
+    		FileWriter fw=new FileWriter(fichier);
+    		fw.write("3\n0\n3\n1 2\n");
+    		fw.close();
+    	} catch (Exception e){}
     }
 }
