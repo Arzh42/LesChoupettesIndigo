e1fe41558571a3b38d0397c161a657b4df3ed82a
Unknown <louismaximepiton@gmail.com>
2019-02-28 19:35:43 +0100

    Tets

--- Projet/src/com/company/Main.java
+++ Projet/src/com/company/Main.java
@@ -1,25 +1,36 @@
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.File;
+import java.util.*;
+
+package com.company;
 
 public class Main {
 
     public static void main(String[] args) {
-        String file = "4\n" +
-        "H 3 cat beach sun\n" +
-        "V 2 selfie smile\n" +
-        "V 2 garden selfie\n" +
-        "H 2 garden cat";
+    	InputStream is = new FileInputStream("a_example.txt");
+    	BufferedReader buf = new BufferedReader(new InputStreamReader(is));
+        
+		String line = buf.readLine();
+		StringBuilder sb = new StringBuilder();
+        
+		while(line != null){
+   			sb.append(line).append("\n");
+   			line = buf.readLine();
+		}
+
+
+
+        String file = sb.toString();
         String[] ligne = file.split("\n");
 
         Parser p = new Parser();
-        p.parse(file);
-    	try{
-    		File fichier = new File("./A.txt");
-    		fichier.createNewFile();
-    		FileWriter fw=new FileWriter(fichier);
-    		fw.write("3\n0\n3\n1 2\n");
-    		fw.close();
-    	} catch (Exception e){}
+        ArrayList<Photo> A = p.parse(file);
+
+        Collections.sort(A, new Comparator<Photo>(){
+        	public int compare(Photo p1, Photo p2){
+        		return p1.getNtag() > p2.getNtag();
+        	}
+        } );
     }
 }
